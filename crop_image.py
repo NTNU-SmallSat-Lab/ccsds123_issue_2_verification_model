76673c46d9bf264135b9dfc0cc089c1d1731d4f9
Daniel Vorhaug <dvorhaug@gmail.com>
2023-10-09 09:41:02 +0200

    Write image cropper

---
+++ crop_image.py
@@ -0,0 +1,41 @@
+import numpy as np
+import re
+
+image_name = 'Landsat_mountain-u16be-6x1024x1024.raw'
+new_x_size = 100
+new_y_size = 100
+new_z_size = 6
+
+
+raw_image_folder = "raw_images"
+
+
+def main():
+    x_size = int(re.findall('x(.*).raw', image_name)[0].split("x")[-1]) 
+    y_size = int(re.findall('x(.+)x', image_name)[0])
+    z_size = int(re.findall('-(.+)x', re.findall('-(.+)x', image_name)[0])[0])
+
+    file = open(raw_image_folder + "/" + image_name, 'rb').read()
+    first = file[0] # Set aside first byte
+    last = (file[-2] << 8) + file[-1] # set aside last two bytes
+    with open(raw_image_folder + "/" + image_name, 'rb') as file:
+        file.seek(1) # Skip the first byte
+        image_sample = np.fromfile(file, dtype=np.uint16)
+    image_sample=np.pad(image_sample, (0,1), 'constant', constant_values=last) # Pad the last two bytes
+    image_sample[0] += first << 8 # Reintroduce the first byte
+    image_sample = image_sample.reshape((z_size, y_size, x_size))
+
+    new_image = image_sample[:new_z_size, :new_y_size, :new_x_size]
+
+    new_image = new_image.reshape(new_z_size * new_y_size * new_x_size)
+    # print(new_image.shape)
+
+    new_image_name = raw_image_folder + "/" + image_name.split(f"-{z_size}x")[0] + f"-{new_z_size}x{new_y_size}x{new_x_size}.raw"
+    # np.save(new_image_name, new_image)
+    with open(new_image_name, 'wb') as file:
+        file.write(bytes([0]))
+        file.write(new_image.tobytes())
+
+if __name__ == "__main__":
+    main()
+
