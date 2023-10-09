600468d0d99724d1c90b676129849ab7a45b0819
Daniel Vorhaug <dvorhaug@gmail.com>
2023-10-09 09:41:11 +0200

    Optimize image displayer

--- display_image.py
+++ display_image.py
@@ -1,52 +1,46 @@
-import rawpy
-import imageio
-from matplotlib import pyplot as plt
 from PIL import Image
 import re
-from math import sqrt
+import numpy as np
 
-display_layer = 128
-imageName = 'Landsat_mountain-u16be-6x1024x1024.raw'
-imageName = 'aviris_hawaii_f011020t01p03r05_sc01.uncal-u16be-224x512x614.raw'
-# imageName = 'aviris_maine_f030828t01p00r05_sc10.uncal-u16be-224x512x680.raw'
+display_layer = 1 # 1-indexed
+# image_name = 'aviris_hawaii_f011020t01p03r05_sc01.uncal-u16be-224x512x614.raw'
+image_name = 'aviris_maine_f030828t01p00r05_sc10.uncal-u16be-224x512x680.raw'
+image_name = 'Landsat_mountain-u16be-6x100x100.raw'
+image_name = 'Landsat_mountain-u16be-6x1024x1024.raw'
 
 
 # I am a regex master
-imageXcount = int(re.findall('x(.*).raw', imageName)[0].split("x")[-1]) 
-imageYcount = int(re.findall('x(.+)x', imageName)[0])
-imageZcount = int(re.findall('-(.+)x', re.findall('-(.+)x', imageName)[0])[0])
-imageType = re.findall('-(.*)-', imageName)[0]
+x_size = int(re.findall('x(.*).raw', image_name)[0].split("x")[-1]) 
+y_size = int(re.findall('x(.+)x', image_name)[0])
+z_size = int(re.findall('-(.+)x', re.findall('-(.+)x', image_name)[0])[0])
+imageType = re.findall('-(.*)-', image_name)[0]
 
-# print(imageXcount)
-# print(imageYcount)
-# print(imageZcount)
+# print(x_size)
+# print(y_size)
+# print(z_size)
 # print(imageType)
 
-rawFolder = "raw_images"
-
-path = rawFolder + "/" + imageName
-f = open(path, 'rb').read()
-
-# for i in range(40):
-#     print(f[i], end=" ")
-# print("")
-# offset = 2*1024
-# for i in range(offset, offset+40):
-#     print(f[i], end=" ")
+raw_image_folder = "raw_images"
 
 
+file = open(raw_image_folder + "/" + image_name, 'rb').read()
+first = file[0] # Set aside first byte
+last = (file[-2] << 8) + file[-1] # set aside last two bytes
+with open(raw_image_folder + "/" + image_name, 'rb') as file:
+    file.seek(1) # Skip the first byte
+    image_sample = np.fromfile(file, dtype=np.uint16)
+if image_sample.shape[0] != z_size * y_size * x_size:
+    image_sample=np.pad(image_sample, (0,1), 'constant', constant_values=last) # Pad the last two bytes
+image_sample[0] += first << 8 # Reintroduce the first byte
+image_sample = image_sample.reshape((z_size, y_size, x_size))
 
-index = 0 + 2 * imageXcount * imageYcount * display_layer
 
-img = Image.new('RGB', (imageXcount, imageYcount), "black")  # Create a new black image
+img = Image.new('RGB', (x_size, y_size), "black")  # Create a new black image
 pixels = img.load()  # Create the pixel map
+
 for y in range(img.size[1]):    # For every pixel:
     for x in range(img.size[0]):
-        if imageType == "u16be":
-            pixelVal = (f[index] << 8) + f[index+1]
-        else:
-            exit("Unknown image type")
-        pixels[x, y] = (pixelVal,pixelVal,pixelVal)
-        index += 2
+        val = image_sample[display_layer-1, y, x]
+        pixels[x, y] = (val,val,val)
 
 img.show()
